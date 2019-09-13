e1c58bce09de9e0883173cde8c6d87c27200aec6
aurea munoz <amunozhe@redhat.com>
2019-09-13 18:41:52 +0200

    test clean tree detection

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # This is a hello world readme!!!
+Let s complete the readme
--- scripts/tag_release_manually.sh
+++ scripts/tag_release_manually.sh
@@ -9,13 +9,35 @@ AUTH="Authorization: token $GITHUB_API_TOKEN"
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$OWNER/$REPO"
 
-if [[ -z $(git status -s) ]]
-then
-  echo "tree is clean"
-else
-  echo "tree is dirty, please commit changes before running this"
-  exit
-fi
+require_clean_work_tree () {
+    # Update the index
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    # Disallow unstaged changes in the working tree
+    if ! git diff-files --quiet --ignore-submodules --
+    then
+        echo >&2 "cannot $1: you have unstaged changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        err=1
+    fi
+
+    # Disallow uncommitted changes in the index
+    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        echo >&2 "cannot $1: your index contains uncommitted changes."
+        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        echo >&2 "Please commit or stash them."
+        exit 1
+    fi
+}
+
+require_clean_work_tree
 
 #echo "Tagging ..."
 #git tag -a $TAG_ID -m "$TAG_ID"
